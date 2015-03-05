87d0c3836a9eaf8e5c27bea761d1ab3ebeba0484
Casey Slaught <cms782@nyu.edu>
2015-03-05 15:54:56 -0500

    new homepage

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', 
+  	{ title: 'Express',
+  	  subtitle:'Casey' });
 });
+
+
 
 module.exports = router;
