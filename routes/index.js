bb25aa0b77ac6958c66a3c605603c16e34968641
Casey Slaught <cms782@nyu.edu>
2015-03-05 19:23:19 -0500

    floppity flop

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,9 @@ router.get('/', function(req, res, next) {
 /*Handle POST requests -- from FrontlineSMS ??*/
 
 router.get("/getfish", function(req, res, next) {
+	//request content will be JSON
+
+
 	/*
 	1) get secret key (maybe a work that can be easily memorized??)
 	2) get rest of message: "buy/sell <type of fish> amount(kg) price"
@@ -18,13 +21,19 @@ router.get("/getfish", function(req, res, next) {
 	4) if not valid -- respond with "key not valid"
 	5) run matching algorithm to get 5-10 matching bid/ask
 		-- "buy/sell <type of fish> amount(kg) price location phone#"
-	6) return message (res.end(message)) ??
+	6) return message (res.end(message)) ?? 
+		- return message payload should be JSON
+			-includes:
+				1) API secret //can probably be left out
+				2) message //try to limit < 160 chars, 
+				3) recipients //array of entities ('type':value)
+					- probably recipient:[{"type":"address", "value":"+1234567890"}]
+
+	7) handle responses
 	*/
 
 	//just use this for now
-	res.send("hello world");
-
-
+	res.send("floppity flop");
 })
 
 
